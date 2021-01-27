6fe130d5d04abfa8d8c18b802f9f26bdbd9900ec
Rob Farlow <robfarlow@gmail.com>
2021-01-27 13:13:05 -0500

    Fix

--- lib/help_scout/api.rb
+++ lib/help_scout/api.rb
@@ -35,7 +35,7 @@ module HelpScout
         HelpScout::Response.new(result)
       else
         case result.status
-        when 400 then raise BadRequest.new result
+        when 400 then raise BadRequest.new result.body
         when 401 then raise NotAuthorized, result.body&.dig('error_description')
         when 404 then raise NotFound, 'Resource Not Found'
         when 429 then raise ThrottleLimitReached, result.body&.dig('error')
